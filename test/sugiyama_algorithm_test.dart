d4c34f2322196dc8d14238d2a03d65958168e68a
Nabil Mosharraf <nabil.mosharraf@photobookworldwide.com>
2021-11-24 04:06:11 +0800

    Added Sugiyama Algorithm Tests

---
+++ test/sugiyama_algorithm_test.dart
@@ -0,0 +1,234 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:graphview/GraphView.dart';
+import 'package:flutter_test/flutter_test.dart';
+const itemHeight = 100.0;
+const itemWidth = 100.0;
+
+void main() {
+  group('Sugiyama Graph', () {
+
+    final graph  = Graph();
+    final node1 = Node.Id(1);
+    final node2 = Node.Id(2);
+    final node3 = Node.Id(3);
+    final node4 = Node.Id(4);
+    final node5 = Node.Id(5);
+    final node6 = Node.Id(6);
+    final node8 = Node.Id(7);
+    final node7 = Node.Id(8);
+    final node9 = Node.Id(9);
+    final node10 = Node.Id(10);
+    final node11 = Node.Id(11);
+    final node12 = Node.Id(12);
+    final node13 = Node.Id(13);
+    final node14 = Node.Id(14);
+    final node15 = Node.Id(15);
+    final node16 = Node.Id(16);
+    final node17 = Node.Id(17);
+    final node18 = Node.Id(18);
+    final node19 = Node.Id(19);
+    final node20 = Node.Id(20);
+    final node21 = Node.Id(21);
+    final node22 = Node.Id(22);
+    final node23 = Node.Id(23);
+
+    graph.addEdge(node1, node13, paint: Paint()..color = Colors.red);
+    graph.addEdge(node1, node21);
+    graph.addEdge(node1, node4);
+    graph.addEdge(node1, node3);
+    graph.addEdge(node2, node3);
+    graph.addEdge(node2, node20);
+    graph.addEdge(node3, node4);
+    graph.addEdge(node3, node5);
+    graph.addEdge(node3, node23);
+    graph.addEdge(node4, node6);
+    graph.addEdge(node5, node7);
+    graph.addEdge(node6, node8);
+    graph.addEdge(node6, node16);
+    graph.addEdge(node6, node23);
+    graph.addEdge(node7, node9);
+    graph.addEdge(node8, node10);
+    graph.addEdge(node8, node11);
+    graph.addEdge(node9, node12);
+    graph.addEdge(node10, node13);
+    graph.addEdge(node10, node14);
+    graph.addEdge(node10, node15);
+    graph.addEdge(node11, node15);
+    graph.addEdge(node11, node16);
+    graph.addEdge(node12, node20);
+    graph.addEdge(node13, node17);
+    graph.addEdge(node14, node17);
+    graph.addEdge(node14, node18);
+    graph.addEdge(node16, node18);
+    graph.addEdge(node16, node19);
+    graph.addEdge(node16, node20);
+    graph.addEdge(node18, node21);
+    graph.addEdge(node19, node22);
+    graph.addEdge(node21, node23);
+    graph.addEdge(node22, node23);
+    graph.addEdge(node1, node22);
+    graph.addEdge(node7, node8);
+
+    test('Sugiyama Node positions are correct for Top_Bottom', () {
+
+    final _configuration = SugiyamaConfiguration()
+        ..nodeSeparation = 15
+        ..levelSeparation = 15
+        ..orientation = SugiyamaConfiguration.ORIENTATION_TOP_BOTTOM;
+
+     var algorithm =  SugiyamaAlgorithm(_configuration);
+
+      for(var i = 0 ; i < graph.nodeCount(); i++ ) {
+        graph.getNodeAtPosition(i).size = Size(itemWidth, itemHeight);
+      }
+
+      var stopwatch = Stopwatch()..start();
+      var size = algorithm.run(graph, 10, 10);
+      var timeTaken = stopwatch.elapsed.inMilliseconds;
+
+      print('Timetaken $timeTaken');
+
+      expect(timeTaken < 1000, true);
+
+      expect(graph.getNodeAtPosition(0).position, Offset(585, 10));
+      expect(graph.getNodeAtPosition(6).position, Offset(1045.0, 815.0));
+      expect(graph.getNodeAtPosition(13).position, Offset(1045.0, 470.0));
+      expect(graph.getNodeAtPosition(22).position, Offset(700, 930.0));
+      expect(graph.getNodeUsingId(3).position, Offset(815.0, 125.0));
+      expect(graph.getNodeUsingId(4).position, Offset(585.0, 240.0));
+
+      expect(size, Size(1365.0, 1135.0));
+    });
+
+
+    test('Sugiyama Node positions correct for LEFT_RIGHT', () {
+      final _configuration = SugiyamaConfiguration()
+        ..nodeSeparation = 15
+        ..levelSeparation = 15
+        ..orientation = SugiyamaConfiguration.ORIENTATION_LEFT_RIGHT;
+
+      var algorithm =  SugiyamaAlgorithm(_configuration);
+
+      for(var i = 0 ; i < graph.nodeCount(); i++ ) {
+        graph.getNodeAtPosition(i).size = Size(itemWidth, itemHeight);
+      }
+
+      var stopwatch = Stopwatch()..start();
+      var size = algorithm.run(graph, 10, 10);
+      var timeTaken = stopwatch.elapsed.inMilliseconds;
+
+      print('Timetaken $timeTaken');
+
+      expect(timeTaken < 1000, true);
+
+      expect(graph.getNodeAtPosition(0).position, Offset(10, 385.0));
+      expect(graph.getNodeAtPosition(6).position, Offset(815.0, 745.0));
+      expect(graph.getNodeAtPosition(13).position, Offset(470.0, 745.0));
+      expect(graph.getNodeAtPosition(22).position, Offset(930, 500.0));
+      expect(graph.getNodeUsingId(3).position, Offset(125.0, 465.0));
+      expect(graph.getNodeUsingId(4).position, Offset(240.0, 342.5));
+
+      expect(size, Size(1135.0, 865.0));
+    });
+
+
+    test('Sugiyama Performance for unconnected nodes', () {
+      final graph  = Graph();
+
+      graph.addEdge(Node.Id(1), Node.Id(3));
+      graph.addEdge(Node.Id(4), Node.Id(7));
+
+      final _configuration = SugiyamaConfiguration()
+        ..nodeSeparation = 15
+        ..levelSeparation = 15
+        ..orientation = SugiyamaConfiguration.ORIENTATION_LEFT_RIGHT;
+
+      var algorithm =  SugiyamaAlgorithm(_configuration);
+
+      for(var i = 0 ; i < graph.nodeCount(); i++ ) {
+        graph.getNodeAtPosition(i).size = Size(itemWidth, itemHeight);
+      }
+
+      var stopwatch = Stopwatch()..start();
+      var size = algorithm.run(graph, 10, 10);
+      var timeTaken = stopwatch.elapsed.inMilliseconds;
+
+      expect(timeTaken < 1000, true);
+
+      expect(graph.getNodeUsingId(1).position, Offset(10.0, 10.0));
+      expect(graph.getNodeUsingId(3).position, Offset(125.0, 10.0));
+
+      expect(size, Size(215.0, 215.0));
+    });
+
+    test('Sugiyama for a single directional graph', () {
+      final graph  = Graph();
+
+      graph.addEdge(Node.Id(1), Node.Id(3));
+      graph.addEdge(Node.Id(3), Node.Id(4));
+      graph.addEdge(Node.Id(4), Node.Id(7));
+      graph.addEdge(Node.Id(7), Node.Id(9));
+      graph.addEdge(Node.Id(9), Node.Id(111));
+
+      final _configuration = SugiyamaConfiguration()
+        ..nodeSeparation = 15
+        ..levelSeparation = 15
+        ..orientation = SugiyamaConfiguration.ORIENTATION_LEFT_RIGHT;
+
+      var algorithm =  SugiyamaAlgorithm(_configuration);
+
+      for(var i = 0 ; i < graph.nodeCount(); i++ ) {
+        graph.getNodeAtPosition(i).size = Size(itemWidth, itemHeight);
+      }
+
+      var stopwatch = Stopwatch()..start();
+      var size = algorithm.run(graph, 10, 10);
+      var timeTaken = stopwatch.elapsed.inMilliseconds;
+
+      expect(timeTaken < 1000, true);
+
+      expect(graph.getNodeUsingId(1).position, Offset(10.0, 10.0));
+      expect(graph.getNodeUsingId(3).position, Offset(125.0, 10.0));
+      expect(graph.getNodeUsingId(9).position, Offset(470.0, 10.0));
+
+      expect(size, Size(675.0, 100.0));
+    });
+
+    test('Sugiyama for a cyclic graph', () {
+      final graph  = Graph();
+
+      graph.addEdge(Node.Id(1), Node.Id(3));
+      graph.addEdge(Node.Id(3), Node.Id(4));
+      graph.addEdge(Node.Id(4), Node.Id(7));
+      graph.addEdge(Node.Id(7), Node.Id(9));
+      graph.addEdge(Node.Id(9), Node.Id(1));
+
+      final _configuration = SugiyamaConfiguration()
+        ..nodeSeparation = 15
+        ..levelSeparation = 15
+        ..orientation = SugiyamaConfiguration.ORIENTATION_LEFT_RIGHT;
+
+      var algorithm =  SugiyamaAlgorithm(_configuration);
+
+      for(var i = 0 ; i < graph.nodeCount(); i++ ) {
+        graph.getNodeAtPosition(i).size = Size(itemWidth, itemHeight);
+      }
+
+      var stopwatch = Stopwatch()..start();
+      var size = algorithm.run(graph, 10, 10);
+      var timeTaken = stopwatch.elapsed.inMilliseconds;
+
+      expect(timeTaken < 1000, true);
+
+      expect(graph.getNodeUsingId(1).position, Offset(10.0, 17.5));
+      expect(graph.getNodeUsingId(3).position, Offset(125.0, 10.0));
+      expect(graph.getNodeUsingId(9).position, Offset(470.0, 67.5));
+
+      expect(size, Size(560.0, 157.5));
+    });
+
+
+  });
+}
